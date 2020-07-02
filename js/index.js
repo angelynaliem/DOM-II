472d20655ff793088044ad1dcdd5ecb89a42dfe5
angelynaliem <angelynaliem@gmail.com>
2020-07-01 23:28:09 -0400

    Added 10 event listeners

--- js/index.js
+++ js/index.js
@@ -15,20 +15,72 @@ intro.addEventListener('dblclick', (event) => {
     event.target.style.cursor = "zoom-in"
 })
 
-// let image1 = document.querySelector(".img-content");
-
-// image1.addEventListener('keydown', (event) => {
-//     event.target.style.opacity= "0.5"
-// })
-
-// window.ononline = (event) => {
-//     console.log("You are now connected to the network.");
-//   };
-
 //event listener #3
+let nav = document.querySelector("nav");
+
+nav.addEventListener("focusin", (event) => {
+    event.target.style.fontSize = "4rem"
+})
+
+//event litener #4
+nav.addEventListener("focusout", (event) => {
+    event.target.style.textDecoration = "none"
+})
+
+//event listener #5
 window.ononline = (event) => {
     alert("You are now connected to the network!")
 }
 
-//event listener #4
+//event listener #6
+window.addEventListener("offline", (event) => {
+    alert("You are not connected to the network!")
+})
 
+//event listener #7
+window.addEventListener("resize", (event) => {
+    alert("Just playing with resizing")
+})
+
+//event listener #8
+let last_known_scroll_position = 0;
+let ticking = false;
+
+function doSomething(scroll_pos) {
+    alert("Playing with scrolling")
+}
+
+window.addEventListener("scroll", function(e) {
+    last_known_scroll_position = window.scrollY;
+
+    if (!ticking) {
+        window.requestAnimationFrame(function() {
+            doSomething(last_known_scroll_position);
+            ticking = false;
+        })
+        ticking = true;
+    }
+})
+
+//event listener #9
+let textContent = document.querySelector(".text-content");
+
+textContent.addEventListener("copy", (event) => {
+    event.target.style.textShadow = "5px 5px 1px #ff0000"
+})
+
+//event listener #10
+let p = document.querySelector(".intro p");
+
+document.addEventListener("keydown", logKey);
+
+function logKey(e) {
+    p.textContent += "Hello";
+}
+
+
+// document.addEventListener('keydown', logKey);
+
+// function logKey(e) {
+//   log.textContent += ` ${e.code}`;
+// }
